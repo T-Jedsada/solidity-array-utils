63cb5fc8b45d90d5a13a210da5d34766c7efd69a
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-07-19 18:00:45 +0700

    add func find

--- lib/List.sol
+++ lib/List.sol
@@ -6,14 +6,28 @@ library List {
         uint[] items;
     }
 
-    function pushItem(Items storage data, uint value) public {
+    function addItem(Items storage data, uint value) public {
         data.items.push(value);
     }
 
-    function popItem(Items storage data) public returns (uint[]) {
+    function removeItem(Items storage data) public returns (uint[]) {
         delete data.items[data.items.length-1];
         data.items.length--;
         return data.items;
+    }
+
+    function removeItem(Items storage data, uint index) public returns (uint[]) {
+        delete data.items[index];
+        data.items.length--;
+        return data.items;
+    }
+
+    function findItem(Items storage data, uint value) public returns (uint) {
+        for (var index=0; index < data.items.length; index++) {
+            if (data.items[index] == value) {
+                return index;
+            }
+        }
     }
     
     function getSize(Items storage data) public view returns (uint) {
