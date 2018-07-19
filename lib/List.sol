319eb00d5ca3932cf38e4d5f9106f1233b799220
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-07-19 18:24:21 +0700

    add sort

--- lib/List.sol
+++ lib/List.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.16;
 
 library List {
 
+    modifier restricted(Items storage data) {
+        require(data.items.length > 0);
+        _;
+    }
+
     struct Items {
         uint[] items;
     }
@@ -22,12 +27,33 @@ library List {
         return data.items;
     }
 
-    function findItem(Items storage data, uint value) public returns (uint) {
-        for (var index=0; index < data.items.length; index++) {
+    function findItem(Items storage data, uint value) public restricted(data) returns (uint) {
+        for (uint index = 0; index < data.items.length; index++) {
             if (data.items[index] == value) {
                 return index;
             }
         }
+    }
+
+    function sort(Items storage data) public returns (uint[]) {
+        uint length = data.items.length;
+        uint[] memory arr = new uint[] (length);
+
+        for (uint index = 0; i < length; index++) {
+            arr[i] = data.items[i];
+        }
+
+        for (uint i = 0; i < length; i++) {
+            for (uint j = i + 1; j < length; j++) {
+                if (arr[i] < arr[j]) {
+                    uint temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }   
+        }
+
+        return arr;
     }
     
     function getSize(Items storage data) public view returns (uint) {
